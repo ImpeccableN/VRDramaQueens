eca0412ce911743409601b100eb7cc11d09b6b12
ImpeccableN <krakatauerich@web.de>
2023-11-15 15:44:22 +0100

    experiment with animation

--- addons/godot-xr-tools/objects/interactable_body.gd
+++ addons/godot-xr-tools/objects/interactable_body.gd
@@ -7,3 +7,8 @@ signal pointer_released(at)
 signal pointer_moved(from, to)
 signal pointer_entered()
 signal pointer_exited()
+
+
+
+func _on_NoraStandIn_pointer_pressed(at):
+	$Barbarian/AnimationPlayer.play("2H_Melee_Attack_Spin")
