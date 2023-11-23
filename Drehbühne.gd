61ffb489b597066dfb6814e9b18884092b10c6b0
ImpeccableN <krakatauerich@web.de>
2023-11-23 18:39:34 +0100

    experimented with hinge steps

--- Drehbühne.gd
+++ Drehbühne.gd
@@ -4,4 +4,4 @@ extends StaticBody
 
 func _on_InteractableHinge_hinge_moved(angle, hinge_limit_min, hinge_limit_max):
 	angle = -angle
-	rotation_degrees.y = ((angle + hinge_limit_max) / hinge_limit_max) * 360
+	rotation_degrees.y = ((angle + hinge_limit_max) / hinge_limit_max) * 180
