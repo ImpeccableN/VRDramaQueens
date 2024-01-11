0b6747befdfc0e417056f858b2db5dc04dbd1aff
ImpeccableN <krakatauerich@web.de>
2024-01-11 17:29:10 +0100

    implemented dimming lights

--- Objects/Interactables/Lichtpult.gd
+++ Objects/Interactables/Lichtpult.gd
@@ -17,11 +17,19 @@ func _ready():
 	var button2 = $Button2/RedButton/InteractableAreaButton
 	var button3 = $Button3/RedButton/InteractableAreaButton
 	
+	var lever1 = $Lever1/LeverOrigin/InteractableHinge
+	var lever2 = $Lever2/LeverOrigin/InteractableHinge
+	var lever3 = $Lever3/LeverOrigin/InteractableHinge
+	
 	
 	off_button.connect("button_pressed", self, "_OffButton_pressed")
 	button1.connect("button_pressed", self, "_OnButton1_pressed")
 	button2.connect("button_pressed", self, "_OnButton2_pressed")
 	button3.connect("button_pressed", self, "_OnButton3_pressed")
+	lever1.connect("hinge_moved", self, "_OnHinge1_moved")
+	lever2.connect("hinge_moved", self, "_OnHinge2_moved")
+	lever3.connect("hinge_moved", self, "_OnHinge3_moved")
+	
 
 
 func _OffButton_pressed(button):
@@ -37,3 +45,14 @@ func _OnButton2_pressed(_button):
 
 func _OnButton3_pressed(_button):
 	_connectedLight3.lamp_switch()
+
+func _OnHinge1_moved(angle):
+	_connectedLight1.dim_light(angle)
+	
+func _OnHinge2_moved(angle):
+	_connectedLight2.dim_light(angle)
+	
+func _OnHinge3_moved(angle):
+	_connectedLight3.dim_light(angle)
+
+
--- Objects/Scenes/Spotlight2.gd
+++ Objects/Scenes/Spotlight2.gd
@@ -10,3 +10,6 @@ func lamp_switch():
 		lamp.light_energy = 0
 	else:
 		lamp.light_energy = temp_light_energy
+
+func dim_light(angle):
+	lamp.light_energy = ((angle + 45) / 90) * -1
