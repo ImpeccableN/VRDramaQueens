678b4094347b7b56cf51f6417e0ceb3c7f662f48
ImpeccableN <krakatauerich@web.de>
2024-02-29 15:51:09 +0100

    worked on OnButton of Lichtpult

---
+++ Objects/Scenes/LichtpultSpawnPoint.gd
@@ -0,0 +1,13 @@
+extends Spatial
+
+export var spawnpointObject : PackedScene
+
+onready var instancedLichtpult = preload("res://Objects/Interactables/Lichtpult.tscn")
+
+var spawnpointOrigin : Vector3
+
+func _on_InteractableAreaButton_button_pressed(button):
+	spawnpointOrigin = spawnpointObject.transform.origin
+	var newLichtpult = instancedLichtpult.instance()
+	add_child(newLichtpult)
+	newLichtpult.transform.origin = spawnpointOrigin
