4a24ee7a9da943d6fe1a7029905823529a74f6c5
ImpeccableN <krakatauerich@web.de>
2024-03-01 17:53:27 +0100

    workd on Off and On button

---
+++ Objects/Interactables/OffButton.gd
@@ -0,0 +1,8 @@
+extends Spatial
+
+onready var onButton = get_node("/LichtpultPickable/OnButton")
+
+signal no_lichtpult_exists
+
+func emitsignal():
+	emit_signal("no_lichtpult_exists")
--- Objects/Scenes/LichtpultSpawnPoint.gd
+++ Objects/Scenes/LichtpultSpawnPoint.gd
@@ -4,6 +4,8 @@ export var spawnpointObject : PackedScene
 
 onready var instancedLichtpult = preload("res://Objects/Interactables/Lichtpult.tscn")
 
+signal on_button_active
+
 var spawnpointOrigin : Vector3
 
 func _on_InteractableAreaButton_button_pressed(button):
