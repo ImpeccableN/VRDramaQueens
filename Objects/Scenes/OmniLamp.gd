f6789ce99f002e19df674206028c0b3af837ac1d
ImpeccableN <krakatauerich@web.de>
2024-03-09 19:39:42 +0100

    BETA for Export Test

--- Objects/Scenes/OmniLamp.gd
+++ Objects/Scenes/OmniLamp.gd
@@ -2,14 +2,18 @@ extends Spatial
 
 onready var lamp := $OmniLight
 
-var temp_light_energy = 0.0
+var temp_light_energy = 0
+var lamp_on = true
 
 func lamp_switch():
 	if lamp.light_energy > 0:
 		temp_light_energy = lamp.light_energy
 		lamp.light_energy = 0
+		lamp_on = false
 	else:
 		lamp.light_energy = temp_light_energy
+		lamp_on = true
 
 func dim_light(angle):
-	lamp.light_energy = ((angle + 45) / 90) * -16
+	if lamp_on:
+		lamp.light_energy = ((angle + 45) / 90)  * 16
