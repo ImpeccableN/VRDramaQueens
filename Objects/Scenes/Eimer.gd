8599d30b1d9d07033375770827b94bfa77c11d66
Berivan <berivan.altunsaray@web.de>
2024-03-11 20:10:16 +0100

    Dialog ohne Szene1

---
+++ Objects/Scenes/Eimer.gd
@@ -0,0 +1,6 @@
+extends Area
+
+
+
+func _on_Eimer_body_entered(_body):
+	monitoring = false
--- Objects/Scenes/LichtpultSpawnPoint.gd
+++ Objects/Scenes/LichtpultSpawnPoint.gd
@@ -1,15 +1,15 @@
 extends Spatial
-
-export var spawnpointObject : PackedScene
-
-onready var instancedLichtpult = preload("res://Objects/Interactables/Lichtpult.tscn")
-
-signal on_button_active
-
-var spawnpointOrigin : Vector3
-
-func _on_InteractableAreaButton_button_pressed(button):
-	spawnpointOrigin = spawnpointObject.transform.origin
-	var newLichtpult = instancedLichtpult.instance()
-	add_child(newLichtpult)
-	newLichtpult.transform.origin = spawnpointOrigin
+#
+#export var spawnpointObject : PackedScene
+#
+#onready var instancedLichtpult = preload("res://Objects/Interactables/Lichtpult.tscn")
+#
+#signal on_button_active
+#
+#var spawnpointOrigin : Vector3
+#
+#func _on_InteractableAreaButton_button_pressed(button):
+#	spawnpointOrigin = spawnpointObject.transform.origin
+#	var newLichtpult = instancedLichtpult.instance()
+#	add_child(newLichtpult)
+#	newLichtpult.transform.origin = spawnpointOrigin
