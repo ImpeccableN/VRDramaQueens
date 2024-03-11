f1803c91cae964ef88e9667fe81ca95bcd3a9a1b
Helena <helenakoester@gmx.de>
2024-03-11 15:21:06 +0100

    Integrate AnimationVideo Scenes Initial & End

--- Global Variables.gd
+++ Global Variables.gd
@@ -15,5 +15,5 @@ func setProgress(numb):
 
 
 func _on_MainAnimationPlayer_animation_finished(anim_name):
-	if anim_name == "Szene7_1" or anim_name == "Szene7_2":
-		get_tree().change_scene("res://Objects/Scenes/Ende.tscn")
+	if anim_name == "Szene7_1":
+		get_tree().change_scene("res://EndScene_Video.tscn")
